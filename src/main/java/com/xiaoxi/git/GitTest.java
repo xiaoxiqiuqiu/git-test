94f8db3ce2690e6d162d1dcf069c617b4e3df54f
xiaoxi <lh_yan1016@163.com>
2023-03-31 14:55:08 +0800

    first commit

---
+++ src/main/java/com/xiaoxi/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.xiaoxi.git;
+
+/**
+ * @Author 小xi
+ * @Create 2023-03-31 12:54
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
